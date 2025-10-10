dba0a18eb9050b1763f60d03d3f4728da2b00615
Enzo Bossé <bosseenzo6@gmail.com>
2025-10-10 10:59:54 +0200

    [FIX] SplineContainer

--- Assets/_Project/Runtime/Scripts/RaceManager/AIRace.cs
+++ Assets/_Project/Runtime/Scripts/RaceManager/AIRace.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Splines;
 
-[RequireComponent(typeof(SplineComponent))]
+[RequireComponent(typeof(SplineContainer))]
 public class AIRace : RaceManager
 {
     [SerializeField] GameObject _aiPrefab;
