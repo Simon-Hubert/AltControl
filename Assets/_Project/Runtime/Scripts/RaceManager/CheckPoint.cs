3c21180d7db782ef8139b7ee0929afd3cc86ed31
Simon-Hubert <s.hubert.mailpro@gmail.com>
2025-10-15 23:43:46 +0200

    [Add] Respawn Mechaniques

--- Assets/_Project/Runtime/Scripts/RaceManager/CheckPoint.cs
+++ Assets/_Project/Runtime/Scripts/RaceManager/CheckPoint.cs
@@ -1,13 +1,18 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private int _index;
     [SerializeField] private bool _isFinishLine;
+    [SerializeField] private float _width;
     
     public int Index => _index;
     public bool IsFinishLine => _isFinishLine;
+    private float _respawnBusTime;
+    
+    private Queue<RespawnHandle> _respawnQueue = new Queue<RespawnHandle>();
 
     private void OnTriggerEnter(Collider other)
     {
@@ -23,4 +28,49 @@ public class CheckPoint : MonoBehaviour
         _index = index;
         _isFinishLine = isFinishLine;
     }
+
+    private void Start() {
+        _respawnBusTime = Resources.Load<ChariotConfig>("ChariotConfig").RespawnBusTimer;
+        StartCoroutine(RespawnRoutine());
+    }
+    
+    private void RespawnQueue() {
+        if(_respawnQueue.Count <= 0 ) return;
+        int toRespawn = Mathf.Min(_respawnQueue.Count, 5);
+
+        if (toRespawn == 1) {
+            _respawnQueue.Dequeue()(transform.position, transform.rotation * Quaternion.Euler(0,90,0));
+            return;
+        }
+        
+        for (int i = 0; i < toRespawn; i++) {
+            Vector3 pos = transform.position + (transform.forward * _width / 2) - (transform.forward * (i * _width) / (toRespawn-1));
+            _respawnQueue.Dequeue()(pos, transform.rotation * Quaternion.Euler(0,90,0));
+        }
+    }
+    
+    public void AddToRespawnQueue(RespawnHandle respawnChar) {
+        _respawnQueue.Enqueue(respawnChar);
+    }
+
+    private IEnumerator RespawnRoutine() {
+        while (true) {
+            yield return new WaitForSeconds(_respawnBusTime);
+            RespawnQueue();
+        }
+    }
+    
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawLine(
+            transform.position + transform.forward * _width / 2,
+            transform.position - transform.forward * _width / 2
+        );
+        
+        for (int i = 0; i < 5; i++) {
+            Vector3 pos = transform.position + (transform.forward * _width / 2) - (transform.forward * i * _width / (5-1));
+            Gizmos.DrawSphere(pos, 0.5f);
+        }
+    }
+
+    
 }
